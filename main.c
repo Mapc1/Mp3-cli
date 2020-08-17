1ab4d7ea4554052124a7221a04a3a3db358459c9
Mapc1 <marcocosta579@gmail.com>
2020-08-17 19:18:43 +0100

    Add bitrate and mono settings

--- main.c
+++ main.c
@@ -3,33 +3,75 @@
 #include <mpg123.h>
 #include <fmt123.h>
 #include <pulse/simple.h>
+#include <string.h>
+
+void strcpyOffset (char *src, char *dst, int offset) {
+    int i;
+    for (i = 0; src[offset] != '\0'; offset++, i++)
+        dst[i] = src[offset];
+    dst[i] = '\0';
+}
+
+char *getModes (int argc,char *argv[], pa_sample_spec *ss) {
+    int i;
+    char *filePATH, dup[128];
+    i = 1;
+
+    for (; i < argc; i++) {
+        if (strcmp (argv[i], "-m") == 0 || strcmp (argv[i], "--mono") == 0){
+            ss->channels = 1;
+            ss->rate *= 2;
+        }
+        else if (strncmp (argv[i], "-b=", 3) == 0){
+            strcpyOffset (argv[i], dup, 3);
+            ss->rate = atoi (dup);
+        }
+        else if (strncmp (argv[i], "--bitrate=", 10) == 0) {
+            strcpyOffset (argv[i], dup, 10);
+            ss->rate = atoi (dup);
+        }
+        else if (argv[i][0] != '-')
+            filePATH = argv[i];
+        else {
+            fprintf (stderr, "Error!!!\nUnknown option: %s\n", argv[i]);
+            exit (1);
+        }
+    }
+
+    return filePATH;
+}
 
 int main (int argc, const char *argv[]) { 
     char buffer[1024];
+    char *filePATH;
     struct mpg123_fmt *format;
     mpg123_handle *handle;
     size_t decoded = 1;
     pa_simple *s;
     pa_sample_spec ss;
 
-    if (argc != 2){
-        fprintf (stderr, "Error!\nThis program requires the path of the mp3 files as an argument!");
+    if (argc == 1){
+        fprintf (stdout, "Usage: mp3-cli [OPTION]... [FILE]...\nPlays an mp3 file.\n     -m, --mono      Set audio channel to mono\n     -b=, --bitrate=  Set bitrate\n");
         goto Exit;
     }
+    
     ss.format = PA_SAMPLE_S16NE;
     ss.channels = 2;
     ss.rate = 44100;
 
-    mpg123_init ();
+    filePATH = getModes (argc, (char **) argv, &ss);
 
+    mpg123_init ();
     handle = mpg123_new (NULL, NULL);
-    mpg123_open_fixed (handle, argv[1], MPG123_STEREO, MPG123_ENC_SIGNED_16);
-    
-    s = pa_simple_new (NULL, "mp3-cli", PA_STREAM_PLAYBACK, NULL, "Audio", &ss, NULL, NULL, NULL);
+
+    mpg123_open_fixed (handle, filePATH, MPG123_STEREO, MPG123_ENC_SIGNED_16);
+    s = pa_simple_new (NULL, "MP3-cli", PA_STREAM_PLAYBACK, NULL, "Cli mp3 player", &ss, NULL, NULL, NULL);
+
+    mpg123_read (handle, buffer, 1024, &decoded);
 
     while (decoded > 0) {
-        mpg123_read (handle, buffer, 1024, &decoded);
         pa_simple_write (s, buffer, decoded, NULL);
+        mpg123_read (handle, buffer, 1024, &decoded);
     }
     
 Exit:
