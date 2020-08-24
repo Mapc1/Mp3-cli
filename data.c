beef7a08c2e2344edb3408daf1c0f9d83df83cfd
Mapc1 <marcocosta579@gmail.com>
2020-08-24 19:28:08 +0100

    Split main.c into data.c

---
+++ data.c
@@ -0,0 +1,47 @@
+#include <pulse/simple.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "data.h"
+
+void printHelp () {
+    printf ("Usage: mp3-cli [OPTION]... [FILE]...\nPlays an mp3 file.\n     -h, --help       Display this message\n     -m, --mono       Set audio channel to mono\n     -b=, --bitrate=  Set bitrate\n");
+}
+
+void strcpytillEqual (char *src, char *dst) {
+    int i;
+    int offset;
+    for (offset = 0; src[offset - 1] != '='; offset++);
+    for (i = 0; src[offset] != '\0'; offset++, i++)
+        dst[i] = src[offset];
+    dst[i] = '\0';
+}
+
+char *getModes (int argc,char *argv[], pa_sample_spec *ss) {
+    int i;
+    char *filePATH, dup[128];
+    i = 1;
+
+    for (; i < argc; i++) {
+        if (strcmp (argv[i], "-m") == 0 || strcmp (argv[i], "--mono") == 0){
+            ss->channels = 1;
+            ss->rate *= 2;
+        }
+        else if (strncmp (argv[i], "-b=", 3) == 0 || strncmp (argv[i], "--bitrate=", 10) == 0){
+            strcpytillEqual (argv[i], dup);
+            ss->rate = atoi (dup);
+        }
+        else if (strcmp (argv[i], "-h") == 0 || strcmp (argv[i], "--help") == 0){
+            printHelp ();
+            exit (0);
+        }
+        else if (argv[i][0] != '-')
+            filePATH = argv[i];
+        else {
+            fprintf (stderr, "Error!!!\nUnknown option: %s\n", argv[i]);
+            exit (1);
+        }
+    }
+
+    return filePATH;
+}
---
+++ data.h
@@ -0,0 +1,3 @@
+void printHelp ();
+void strcpytillEqual (char *src, char *dst);
+char *getModes (int argc,char *argv[], pa_sample_spec *ss);
